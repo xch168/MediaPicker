69d15f5f6b7fe2400499fa74f9f70e01250949e7
徐灿辉 <122565648@qq.com>
2020-03-24 23:53:11 +0800

    init commit

---
+++ app/src/main/java/com/github/xch168/mediapicker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.xch168.mediapicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
